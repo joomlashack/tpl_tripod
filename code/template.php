df0a4d1140d62b658ee61653e8264f473a63b037
jmarkos <markos@joomlashack.com>
2013-09-13 18:40:14 -0600

    adding scrolling menu

--- code/template.php
+++ code/template.php
@@ -31,7 +31,7 @@ defined('_JEXEC') or die('Restricted access');
         var parallax_navmenu_scroll_speed = '<?php echo $this->params->get('navmenu_scroll_speed','2000' ); ?>';
     </script>
 </head>
-<body class="<?php echo $responsive ?>">
+<body class="<?php echo $responsive ?>" id="skrollr-body">
     <?php if ($this->countModules('toolbar')) : ?>
         <!-- menu -->
         <w:nav containerClass="<?php echo $containerClass ?>" rowClass="<?php echo $gridMode;?>" wrapClass="navbar-fixed-top" type="toolbar" name="toolbar" />
@@ -53,83 +53,105 @@ defined('_JEXEC') or die('Restricted access');
         </div>
     <?php endif; ?>
 
-    <?php if ($this->countModules('featured')) : ?>
-        <!-- featured -->
-        <div id="featured" class="<?php echo $containerClass; ?>">
-            <div class="<?php echo $gridMode; ?>">
-                <w:module type="none" name="featured" chrome="xhtml" />
-            </div>
-        </div>
-    <?php endif; ?>
+    <div class="<?php echo $gridMode; ?>">
+        <div class="span12" id="spanRestScrollingMenuWrapper" rel="span<?php echo 12-$scrollingMenuWidth ?>">
 
-    <?php if ($this->countModules('grid-top')) : ?>
-        <!-- grid-top -->
-        <div id="grid-top" class="<?php echo $containerClass; ?>">
-            <div class="<?php echo $gridMode; ?>">
-                <w:module type="<?php echo $gridMode; ?>" name="grid-top" chrome="wrightflexgrid" />
+            <?php if ($this->countModules('featured') || $this->countModules('grid-top') || $this->countModules('grid-top2')) : ?>
+            <div id="parallax_pagefirst">
+                <?php if ($this->countModules('featured')) : ?>
+                    <!-- featured -->
+                    <div id="featured" class="<?php echo $containerClass; ?>">
+                        <div class="<?php echo $gridMode; ?>">
+                            <w:module type="none" name="featured" chrome="xhtml" />
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <?php if ($this->countModules('grid-top')) : ?>
+                    <!-- grid-top -->
+                    <div id="grid-top" class="<?php echo $containerClass; ?>">
+                        <div class="<?php echo $gridMode; ?>">
+                            <w:module type="<?php echo $gridMode; ?>" name="grid-top" chrome="wrightflexgrid" />
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <?php if ($this->countModules('grid-top2')) : ?>
+                    <!-- grid-top2 -->
+                    <div id="grid-top2" class="<?php echo $containerClass; ?>">
+                        <div class="<?php echo $gridMode; ?>">
+                            <w:module type="<?php echo $gridMode; ?>" name="grid-top2" chrome="wrightflexgrid" />
+                        </div>
+                    </div>
+                <?php endif; ?>
             </div>
-        </div>
-    <?php endif; ?>
-    <?php if ($this->countModules('grid-top2')) : ?>
-        <!-- grid-top2 -->
-        <div id="grid-top2" class="<?php echo $containerClass; ?>">
-            <div class="<?php echo $gridMode; ?>">
-                <w:module type="<?php echo $gridMode; ?>" name="grid-top2" chrome="wrightflexgrid" />
+            <?php endif; ?>
+
+            <div class="<?php echo $containerClass ?>">
+                <div id="main-content" class="<?php echo $gridMode; ?>">
+                    <!-- sidebar1 -->
+                    <aside id="sidebar1">
+                        <w:module name="sidebar1" chrome="xhtml" />
+                    </aside>
+                    <!-- main -->
+                    <section id="main">
+                        <?php if ($this->countModules('above-content')) : ?>
+                        <!-- above-content -->
+                        <div id="above-content">
+                            <w:module type="none" name="above-content" chrome="xhtml" />
+                        </div>
+                        <?php endif; ?>
+                        <?php if ($this->countModules('breadcrumbs')) : ?>
+                        <!-- breadcrumbs -->
+                        <div id="breadcrumbs">
+                                <w:module type="single" name="breadcrumbs" chrome="none" />
+                        </div>
+                        <?php endif; ?>
+                        <!-- component -->
+                        <w:content />
+                        <?php if ($this->countModules('below-content')) : ?>
+                        <!-- below-content -->
+                        <div id="below-content">
+                            <w:module type="none" name="below-content" chrome="xhtml" />
+                        </div>
+                        <?php endif; ?>
+                    </section>
+                    <!-- sidebar2 -->
+                    <aside id="sidebar2">
+                        <w:module name="sidebar2" chrome="xhtml" />
+                    </aside>
+                </div>
             </div>
+
+
+            <?php if ($this->countModules('grid-bottom') || $this->countModules('grid-bottom2')) : ?>
+                <div id="parallax_pagelast">
+
+                    <?php if ($this->countModules('grid-bottom')) : ?>
+                        <!-- grid-bottom -->
+                        <div id="grid-bottom" >
+                                <w:module type="<?php echo $gridMode; ?>" name="grid-bottom" chrome="wrightflexgrid" />
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($this->countModules('grid-bottom2')) : ?>
+                        <!-- grid-bottom2 -->
+                        <div id="grid-bottom2" >
+                                <w:module type="<?php echo $gridMode; ?>" name="grid-bottom2" chrome="wrightflexgrid" />
+                        </div>
+                    <?php endif; ?>
+
+                </div>
+            <?php endif; ?>
+
         </div>
-    <?php endif; ?>
-    <div class="<?php echo $containerClass ?>">
-        <div id="main-content" class="<?php echo $gridMode; ?>">
-            <!-- sidebar1 -->
-            <aside id="sidebar1">
-                <w:module name="sidebar1" chrome="xhtml" />
-            </aside>
-            <!-- main -->
-            <section id="main">
-                <?php if ($this->countModules('above-content')) : ?>
-                <!-- above-content -->
-                <div id="above-content">
-                    <w:module type="none" name="above-content" chrome="xhtml" />
-                </div>
-                <?php endif; ?>
-                <?php if ($this->countModules('breadcrumbs')) : ?>
-                <!-- breadcrumbs -->
-                <div id="breadcrumbs">
-                        <w:module type="single" name="breadcrumbs" chrome="none" />
-                </div>
-                <?php endif; ?>
-                <!-- component -->
-                <w:content />
-                <?php if ($this->countModules('below-content')) : ?>
-                <!-- below-content -->
-                <div id="below-content">
-                    <w:module type="none" name="below-content" chrome="xhtml" />
-                </div>
-                <?php endif; ?>
-            </section>
-            <!-- sidebar2 -->
-            <aside id="sidebar2">
-                <w:module name="sidebar2" chrome="xhtml" />
-            </aside>
+        <div class="span<?php echo $scrollingMenuWidth ?>" id="spanScrollingMenuWrapper" style="display:none">
         </div>
     </div>
-    <?php if ($this->countModules('grid-bottom')) : ?>
-        <!-- grid-bottom -->
-        <div id="grid-bottom" >
-                <w:module type="<?php echo $gridMode; ?>" name="grid-bottom" chrome="wrightflexgrid" />
-        </div>
-    <?php endif; ?>
-    <?php if ($this->countModules('grid-bottom2')) : ?>
-        <!-- grid-bottom2 -->
-        <div id="grid-bottom2" >
-                <w:module type="<?php echo $gridMode; ?>" name="grid-bottom2" chrome="wrightflexgrid" />
-        </div>
-    <?php endif; ?>
+
     <?php if ($this->countModules('bottom-menu')) : ?>
         <!-- bottom-menu -->
         <w:nav containerClass="<?php echo $containerClass ?>" rowClass="<?php echo $gridMode;?>" name="bottom-menu" />
     <?php endif; ?>
 
+    <script type='text/javascript' src='<?php echo JURI::root(true) ?>/templates/js_tripod/js/skrollr.min.js'></script>
     <script type='text/javascript' src='<?php echo JURI::root(true) ?>/templates/js_tripod/js/tripod.js'></script>
 
     <!-- footer -->

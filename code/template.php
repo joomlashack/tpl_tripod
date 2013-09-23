598df0054a9b201ac5f3dd2103c4a4ffb50ddd86
jmarkos <markos@joomlashack.com>
2013-09-23 11:46:13 -0600

    adding style to slideshow

--- code/template.php
+++ code/template.php
@@ -18,6 +18,8 @@ defined('_JEXEC') or die('Restricted access');
 <html>
 <head>
     <w:head />
+    <link rel="stylesheet" href="<?php echo JURI::root(true) ?>/templates/js_tripod/css/supersized.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo JURI::root(true) ?>/templates/js_tripod/css/supersized.shutter.css" type="text/css" media="screen" />
     <script type="text/javascript">
         var singlescrollingpage = <?php echo ($this->params->get("singleScrollingPage","1") == "1" ? "true" : "false"); ?>;
         var singlepage_titles = new Array();
@@ -30,8 +32,68 @@ defined('_JEXEC') or die('Restricted access');
         var singlepage_navmenu_scroll_speed = '<?php echo $this->params->get('navmenu_scroll_speed','2000' ); ?>';
     </script>
 
+    <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/jquery.easing.min.js"></script>
+
+    <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/supersized.3.2.7.min.js"></script>
+    <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/supersized.shutter.min.js"></script>
+
+    <script type="text/javascript">
+
+        jQuery(function($){
+
+            $.supersized({
+
+                // Functionality
+                slideshow               :   1,          // Slideshow on/off
+                autoplay                :   1,          // Slideshow starts playing automatically
+                start_slide             :   1,          // Start slide (0 is random)
+                stop_loop               :   0,          // Pauses slideshow on last slide
+                random                  :   0,          // Randomize slide order (Ignores start slide)
+                slide_interval          :   3000,       // Length between transitions
+                transition              :   6,          // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+                transition_speed        :   1000,       // Speed of transition
+                new_window              :   1,          // Image links open in new window/tab
+                pause_hover             :   0,          // Pause slideshow on hover
+                keyboard_nav            :   1,          // Keyboard navigation on/off
+                performance             :   1,          // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+                image_protect           :   1,          // Disables image dragging and right click with Javascript
+
+                // Size & Position
+                min_width               :   0,          // Min width allowed (in pixels)
+                min_height              :   0,          // Min height allowed (in pixels)
+                vertical_center         :   1,          // Vertically center background
+                horizontal_center       :   1,          // Horizontally center background
+                fit_always              :   0,          // Image will never exceed browser width or height (Ignores min. dimensions)
+                fit_portrait            :   1,          // Portrait images will not exceed browser height
+                fit_landscape           :   0,          // Landscape images will not exceed browser width
+
+                // Components
+                slide_links             :   'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+                thumb_links             :   1,          // Individual thumb links for each slide
+                thumbnail_navigation    :   0,          // Thumbnail navigation
+                slides                  :   [           // Slideshow Images
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-1.jpg', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-1.jpg', url : 'http://www.nonsensesociety.com/2011/04/maria-kazvan/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-2.jpg', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-2.jpg', url : 'http://www.nonsensesociety.com/2011/04/maria-kazvan/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-3.jpg', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-3.jpg', url : 'http://www.nonsensesociety.com/2011/04/maria-kazvan/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-1.jpg', title : 'Image Credit: Colin Wojno', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-1.jpg', url : 'http://www.nonsensesociety.com/2011/03/colin/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-2.jpg', title : 'Image Credit: Colin Wojno', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-2.jpg', url : 'http://www.nonsensesociety.com/2011/03/colin/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-3.jpg', title : 'Image Credit: Colin Wojno', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-3.jpg', url : 'http://www.nonsensesociety.com/2011/03/colin/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-1.jpg', title : 'Image Credit: Brooke Shaden', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-1.jpg', url : 'http://www.nonsensesociety.com/2011/06/brooke-shaden/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-2.jpg', title : 'Image Credit: Brooke Shaden', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-2.jpg', url : 'http://www.nonsensesociety.com/2011/06/brooke-shaden/'},
+                                                    {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-3.jpg', title : 'Image Credit: Brooke Shaden', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-3.jpg', url : 'http://www.nonsensesociety.com/2011/06/brooke-shaden/'}
+                                            ],
+
+                // Theme Options
+                progress_bar            :   1,          // Timer for each slide
+                mouse_scrub             :   0
+
+            });
+        });
+
+    </script>
+
 </head>
-<body class="<?php echo $responsive ?>" id="skrollr-body">
+<body class="<?php echo $responsive . $tripodFtBlogClass?>" id="skrollr-body">
     <?php if ($this->countModules('toolbar')) : ?>
         <!-- menu -->
         <w:nav containerClass="<?php echo $containerClass ?>" rowClass="<?php echo $gridMode;?>" wrapClass="navbar-fixed-top" type="toolbar" name="toolbar" />
@@ -40,23 +102,19 @@ defined('_JEXEC') or die('Restricted access');
     <!-- header -->
     <header id="header" class="clearfix">
 
-        <div class="bg-wrapp">
-            <div class="bg-wrapp-inner">
-                <?php if ($bg != '-1' && !$tripodSlideshow) : ?>
-                    <img id="bg-header" src="<?php echo JURI::root(true) . $bg ?>" />
-                <?php endif ?>
-                <?php if ($tripodSlideshow) : ?>
-                    <div id="wrapper-slideshow">
-                            lorem
-                    </div>
-                <?php endif ?>
-            </div>
-        </div>
         <?php if ($bg != '-1' && !$tripodSlideshow) : ?>
+            <div class="bg-wrapp">
+                <div class="bg-wrapp-inner">
+                        <img id="bg-header" src="<?php echo JURI::root(true) . $bg ?>" />
+                </div>
+            </div>
             <div class="bg-filter"></div>
         <?php endif ?>
 
         <?php if ($tripodSlideshow) : ?>
+            <!--Arrow Navigation-->
+            <a id="prevslide" class="load-item"></a>
+            <a id="nextslide" class="load-item"></a>
             <a href="" class="tripod-slideshow-nav-prev"></a>
             <a href="" class="tripod-slideshow-nav-next"></a>
         <?php endif ?>
@@ -73,10 +131,7 @@ defined('_JEXEC') or die('Restricted access');
                     </div>
                 </div>
             <?php endif; ?>
-            <div class="tripod-slideshow-wrapper-icons">
-                <div class="tripod-slideshow-icon"></div>
-                <div class="tripod-slideshow-icon"></div>
-            </div>
+
         </div>
 
     </header>

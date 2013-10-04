e805bdb396a7d470eb57885084b507d940867fad
jmarkos <markos@joomlashack.com>
2013-10-04 15:24:29 -0600

    fixing prev/next slideshow

--- code/template.php
+++ code/template.php
@@ -205,40 +205,43 @@ defined('_JEXEC') or die('Restricted access');
 
 
     <?php if ($tripodSlideshow) : ?>
-        <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/supersized.min.js"></script>
         <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/jquery.easing.min.js"></script>
-
+        <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/supersized.min.js"></script>
+        <script type="text/javascript" src="<?php echo JURI::root(true) ?>/templates/js_tripod/js/supersized.shutter.min.js"></script>
 
         <script type="text/javascript">
 
             jQuery(function(){
                 jQuery.supersized({
                     // Functionality
-                    slideshow : 1, // Slideshow on/off
-                    autoplay : 1, // Slideshow starts playing automatically
-                    start_slide : 1, // Start slide (0 is random)
-                    stop_loop : 0, // Pauses slideshow on last slide
-                    random : 0, // Randomize slide order (Ignores start slide)
-                    slide_interval : 12000, // Length between transitions
-                    transition : 2, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-                    transition_speed : 300, // Speed of transition
-                    new_window : 1, // Image links open in new window/tab
-                    pause_hover : 0, // Pause slideshow on hover
-                    keyboard_nav : 1, // Keyboard navigation on/off
-                    performance : 1, // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-                    image_protect : 1, // Disables image dragging and right click with Javascript
+                    slideshow               :   1,          // Slideshow on/off
+                    autoplay                :   1,          // Slideshow starts playing automatically
+                    start_slide             :   1,          // Start slide (0 is random)
+                    stop_loop               :   0,          // Pauses slideshow on last slide
+                    random                  :   0,          // Randomize slide order (Ignores start slide)
+                    slide_interval          :   12000,      // Length between transitions
+                    transition              :   2,          // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+                    transition_speed        :   300,        // Speed of transition
+                    new_window              :   1,          // Image links open in new window/tab
+                    pause_hover             :   0,          // Pause slideshow on hover
+                    keyboard_nav            :   1,          // Keyboard navigation on/off
+                    performance             :   1,          // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+                    image_protect           :   1,          // Disables image dragging and right click with Javascript
+
                     // Size & Position
-                    min_width : 0, // Min width allowed (in pixels)
-                    min_height : 0, // Min height allowed (in pixels)
-                    vertical_center : 1, // Vertically center background
-                    horizontal_center : 1, // Horizontally center background
-                    fit_always : 0, // Image will never exceed browser width or height (Ignores min. dimensions)
-                    fit_portrait : 1, // Portrait images will not exceed browser height
-                    fit_landscape : 0, // Landscape images will not exceed browser width
+                    min_width               :   0,          // Min width allowed (in pixels)
+                    min_height              :   0,          // Min height allowed (in pixels)
+                    vertical_center         :   1,          // Vertically center background
+                    horizontal_center       :   1,          // Horizontally center background
+                    fit_always              :   0,          // Image will never exceed browser width or height (Ignores min. dimensions)
+                    fit_portrait            :   1,          // Portrait images will not exceed browser height
+                    fit_landscape           :   0,          // Landscape images will not exceed browser width
+
                     // Components
-                    slide_links : 'blank', // Individual links for each slide (Options: false, 'num', 'name', 'blank')
-                    thumb_links : 0, // Individual thumb links for each slide
-                    thumbnail_navigation : 0, // Thumbnail navigation
+                    slide_links             :   'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+                    thumb_links             :   0,          // Individual thumb links for each slide
+                    thumbnail_navigation    :   0,          // Thumbnail navigation
+
                     slides                  :   [           // Slideshow Images
                                                         {image : '<?php echo JURI::root(true) . $slideshowImageOneRute; ?>', title : 'Image Title', thumb : '', url : ''},
                                                         {image : '<?php echo JURI::root(true) . $slideshowImageTwoRute; ?>', title : 'Image Title', thumb : '', url : ''},
@@ -250,11 +253,8 @@ defined('_JEXEC') or die('Restricted access');
                 });
             });
 
-
         </script>
     <?php endif; ?>
-
-
 
        <script type="text/javascript">
         var singlescrollingpage = <?php echo ($this->params->get("singleScrollingPage","1") == "1" ? "true" : "false"); ?>;

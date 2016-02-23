93c5a102877fb2ec2a0fae1342b796b5ed00d48a
jmarkos <markos@joomlashack.com>
2016-02-22 23:34:05 -0600

    fixing featured without leadings articles

--- code/js/tripod.js
+++ code/js/tripod.js
@@ -3,10 +3,12 @@ jQuery(function() {
     if (singlescrollingpage) {
         var resizeClass = "div.blog > div.category-desc";
         var resizeClass2 = "div.blog > div.items-leading > [class^=leading-]";
+        var resizeClass3 = "div.blog > div.container > .items-row";
 
         if (!jQuery('div.blog').length) {
             resizeClass = "div.blog-featured > div.category-desc";
             resizeClass2 = "div.blog-featured > div.items-leading > [class^=leading-]";
+			resizeClass3 = "div.blog-featured > div.container > .items-row";
         }
     }
 
@@ -14,7 +16,7 @@ jQuery(function() {
         var htoolbar = jQuery('div.wrapper-toolbar').height();
         var htop = jQuery('#header').height() + jQuery('div.wrapper-toolbar').height();
 
-        var c = jQuery(resizeClass).length + jQuery(resizeClass2).length;
+        var c = jQuery(resizeClass).length + jQuery(resizeClass2).length + jQuery(resizeClass3).length;
 
         if (c > 0) {
             var elems = [];

2268d1e1c1e79ff8a5b7411d5b3cea31b3c3ab6f
Valentín <htmgarcia@gmail.com>
2016-05-31 15:34:57 -0500

    Load Google fonts in the right way and new version!

--- code/template.php
+++ code/template.php
@@ -18,8 +18,8 @@ defined('_JEXEC') or die('Restricted access');
 <html>
 <head>
     <w:head />
-    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Titillium+Web:400,700' rel='stylesheet' type='text/css'>
 </head>
 
 <body class="<?php echo $responsive . $tripodFtBlogClass . $tripodToolbarDisplayedClass .$pixelClass?>">
